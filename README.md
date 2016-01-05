5e5f1e6866611c521d8457b90e13c0412b8bd42d
Ryan <rang1@asu.edu>
2016-01-04 22:45:35 -0700

    part4

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # first-django
+my first django project.
+
+one tutorial https://docs.djangoproject.com/en/1.9/intro/tutorial03/
--- myFirst/settings.py
+++ myFirst/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls',
 ]
+#cse360test is admin
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
