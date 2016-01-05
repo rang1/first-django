09a61ce2d2887ae9d407b83e064f8173dded9e69
Ryan <rang1@asu.edu>
2016-01-05 16:46:17 -0700

    finished some test cases

--- polls/models.py
+++ polls/models.py
@@ -11,13 +11,13 @@ class Question(models.Model):
    def __str__(self):
        return self.question_text
 
-   def was_puplished_recently(self):
+   def was_published_recently(self):
        now = timezone.now()
        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
-   was_puplished_recently.admin_order_field = 'pub_date'
-   was_puplished_recently.boolean = True
-   was_puplished_recently.short_description = 'Published recently?'
+   was_published_recently.admin_order_field = 'pub_date'
+   was_published_recently.boolean = True
+   was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
